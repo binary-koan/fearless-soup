5bc6205c2d1ae4e7aa89ba3843c27b362c267ae1
Jono Mingard <reason.koan@gmail.com>
2023-12-01 20:11:12 +0000

    Add typing effect for answers

--- app/javascript/components/AnswerDisplay.tsx
+++ app/javascript/components/AnswerDisplay.tsx
@@ -1,11 +1,40 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
+import { randomInteger } from "../utils/randomInteger"
+import { sleep } from "../utils/sleep"
 
 const AnswerDisplay: FC<{ answer: string | null; onReset: () => void }> = ({ answer, onReset }) => {
+  const [displayedAnswer, setDisplayedAnswer] = useState("")
+
+  useEffect(() => {
+    if (!answer) {
+      return
+    }
+
+    const controller = new AbortController()
+
+    const typeText = async () => {
+      let current = ""
+
+      while (current.length < answer.length && !controller.signal.aborted) {
+        current += answer[current.length]
+        setDisplayedAnswer(current)
+
+        await sleep(randomInteger(30, 70))
+      }
+    }
+
+    typeText()
+
+    return () => controller.abort()
+  }, [answer])
+
+  const finishedTyping = displayedAnswer === answer
+
   return (
     <p id="answer-container" className={answer ? "" : "hidden"}>
       <strong>Answer: </strong>
-      {answer}
-      <button style={{ display: "block" }} onClick={onReset}>
+      {displayedAnswer}
+      <button style={{ display: finishedTyping ? "block" : "none" }} onClick={onReset}>
         Ask another question
       </button>
     </p>
