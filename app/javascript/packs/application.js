1ed9cad75a492019b95869b1f072ad3cff65ecf3
Jono Mingard <reason.koan@gmail.com>
2023-12-01 20:04:04 +0000

    Copy styles and meta across from original app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+require("../styles/index.css")
+
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
