2eb98db2d5ff8454a01d199c64d12f7e878b031c
Jono Mingard <reason.koan@gmail.com>
2023-12-01 13:03:06 +0000

    Hook up answering logic to API

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,8 @@ class MainController < ApplicationController
   end
 
   def ask
+    question = AnswerQuestion.new(params[:question]).call
+
+    render json: question.as_json(root: true, only: [:id, :question, :answer])
   end
 end
